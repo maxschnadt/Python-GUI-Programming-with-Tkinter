f2e9cc7a3ed72abe80ddd076836938c2d5140fb3
Max Schnadt <76456650+maxschnadt@users.noreply.github.com>
2021-05-22 16:28:12 +0200

    Update better_hello_tkinter.py

--- Chapter01/better_hello_tkinter.py
+++ Chapter01/better_hello_tkinter.py
@@ -18,7 +18,7 @@ class HelloView(tk.Frame):
         name_entry = ttk.Entry(self, textvariable=self.name)
         ch_button = ttk.Button(self, text="Change", command=self.on_change)
         hello_label = ttk.Label(self, textvariable=self.hello_string,
-            font=("TkDefaultFont", 64), wraplength=600)
+                                font=("TkDefaultFont", 64), wraplength=600)
 
         # Layout form
         name_label.grid(row=0, column=0, sticky=tk.W)
